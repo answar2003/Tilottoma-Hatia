31a9f6b71f6bd303c15f82e00bebb918c85bfc63
Mynul Alam <mynulalam27@gmail.com>
2025-03-06 00:03:11 +0600

    delete_lol

--- lib/widgets/Gap10.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-SizedBox Gap() {
-  return SizedBox(
-    width: 10,
-  );
-}
--- lib/widgets/style.dart
+++
@@ -1,6 +0,0 @@
-const inikaBold = "Inika-Bold";
-const inikaRegular = "Inika-Regular";
-const urbanistRegular = "Urbanist-Regular";
-const urbanistBold = "Urbanist-Bold";
-const montRegular = "Montserrat-Regular";
-const montBold = "Montserrat-Bold";
